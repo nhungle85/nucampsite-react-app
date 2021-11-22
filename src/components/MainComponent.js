08519fef0ef3019d1bcb64af90e785d999715b03
nhungle <misssg85@gmail.com>
2021-11-22 12:14:28 -0800

    Adding to the Single Page App

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,24 +1,33 @@
 import React, { Component } from "react";
 import Directory from "./DirectoryComponent";
-import { CAMPSITES } from "../share/campsites";
-import CampsiteInfo from "./CampsiteInfoComponent";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Home from "./HomeComponent";
 import { Redirect, Route, Switch } from "react-router";
+import Contact from "./ContactComponent";
+import { COMMENTS } from '../share/comments';
+import { PARTNERS } from '../share/partners';
+import { PROMOTIONS } from '../share/promotions';
+import { CAMPSITES } from "../share/campsites";
 
 //rec
 export class Main extends Component {
     constructor(props) {
         super(props);
-        this.state = { campsites: CAMPSITES,
-             };
+        this.state = { 
+          campsites: CAMPSITES,
+          comments: COMMENTS,
+          partners: PARTNERS,
+          promotions: PROMOTIONS
+        };
     }
 
     render() {
       const HomePage = () => {
         return(
-          <Home />
+          <Home campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
+                promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
+                partner={this.state.partners.filter(partner => partner.featured)[0]}/>
         );
       };
       return (
@@ -27,6 +36,7 @@ export class Main extends Component {
           <Switch>
             <Route path="/home" component={HomePage} />
             <Route exact path="/directory" render={() => <Directory campsites={this.state.campsites} />} />
+            <Route exact path="/contactus" component={Contact} />
             <Redirect to="/home" />
           </Switch>
           <Footer />
